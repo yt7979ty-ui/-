97ca6a694af7f19d3a887b33be22e64895b3b7d4
yt7979ty-ui <yt79.79ty@gmail.com>
2025-11-12 13:39:41 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('quiz-spec-v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
